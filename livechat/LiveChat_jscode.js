eb021f9c08efda7e2797dae17af2c5e576dbcad0
yjgoh <yjgoh@olimplanet.com>
2022-07-14 10:21:33 +0900

    Create LiveChat_jscode.js
    라이브 채팅 팝업 js code

---
+++ livechat/LiveChat_jscode.js
@@ -0,0 +1,43 @@
+<script>
+  // Channel Plugin Scripts
+  (function() {
+    var w = window;
+    if (w.ChannelIO) {
+      return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+    }
+    var ch = function() {
+      ch.c(arguments);
+    };
+    ch.q = [];
+    ch.c = function(args) {
+      ch.q.push(args);
+    };
+    w.ChannelIO = ch;
+    function l() {
+      if (w.ChannelIOInitialized) {
+        return;
+      }
+      w.ChannelIOInitialized = true;
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+      s.charset = 'UTF-8';
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    }
+    if (document.readyState === 'complete') {
+      l();
+    } else if (window.attachEvent) {
+      window.attachEvent('onload', l);
+    } else {
+      window.addEventListener('DOMContentLoaded', l, false);
+      window.addEventListener('load', l, false);
+    }
+  })();
+  
+  ChannelIO('boot', {
+    "pluginKey": "0aa37504-d583-40a2-a172-90fba8745f12" //여기에 발행된 키값 넣으면 됨
+  });
+
+</script>
