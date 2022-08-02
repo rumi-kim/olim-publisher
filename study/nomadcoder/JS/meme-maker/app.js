f8ee88dd389634357744b344e1002ffb7d165a26
rumi-kim <arkim@olimplanet.com>
2022-08-02 10:16:17 +0900

    update

---
+++ study/nomadcoder/JS/meme-maker/app.js
@@ -0,0 +1,44 @@
+
+const canvas = document.querySelector('canvas');
+
+// context => canvas에 그림을 그릴 수 있는 '붓'이라고 할 수 있음 
+
+const ctx = canvas.getContext('2d');
+
+canvas.width = 800;
+canvas.height = 800;
+
+// make rectangle and fill it 
+
+// ctx.fillRect(50,50,100,200) //fillRect => 단축함수 ( stroke + fill)
+// ctx.rect(50,50,100,100)
+// ctx.rect(150,150,100,100)
+// ctx.rect(250,250,100,100)
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.rect(350,350,100,100)
+// ctx.rect(450,450,100,100)
+// ctx.fillStyle = "red"
+// ctx.fill();
+
+
+
+
+// ctx.rect(50,50,100,100)
+
+
+// moveTo => 선을 긋지 않으면서 위치 이동
+// lineTo => 선을 그으면서  위치 이동
+    
+ctx.moveTo(50,50);
+ctx.lineTo(150,50);
+ctx.lineTo(150,150);
+ctx.lineTo(50,150);
+ctx.lineTo(50,50);
+ctx.fill()
+
+
+
+
+
